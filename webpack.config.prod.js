eeaa09604a011be17ee3407dd6edaa4d1d44a60f
Henry Cruz <ihenvyr@gmail.com>
2016-10-07 20:55:57 +0800

    webpack production config updated (copy-webpack-plugin update)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,8 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var extractCSS = new ExtractTextPlugin('./build/content.css');
+var resolve = require('path').resolve;
 
 module.exports = {
   entry: {
@@ -30,6 +32,9 @@ module.exports = {
         dead_code: true,
         warnings: false
       }
-    })
+    }),
+    new CopyWebpackPlugin([
+      { from: resolve(__dirname, 'src', 'static'), to: resolve(__dirname, 'build') }
+    ])
   ]
 };
